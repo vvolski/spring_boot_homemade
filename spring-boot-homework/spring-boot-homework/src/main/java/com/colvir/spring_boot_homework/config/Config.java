8e06246a446aea7b1bc763cc6ff078cb9fbc29c0
vvolski <valery.volski.colvir@gmail.com>
2024-08-17 00:46:49 +0300

    Настроены миграции через liquibase, подключен кэш чтения выплат через Redis

--- spring-boot-homework/spring-boot-homework/src/main/java/com/colvir/spring_boot_homework/config/Config.java
+++ spring-boot-homework/spring-boot-homework/src/main/java/com/colvir/spring_boot_homework/config/Config.java
@@ -3,6 +3,9 @@ package com.colvir.spring_boot_homework.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -34,5 +37,20 @@ public class Config {
         driverManagerDataSource.setUsername(username);
         driverManagerDataSource.setPassword(password);
         return driverManagerDataSource;
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setEnableDefaultSerializer(false);
+        return template;
     }
 }
